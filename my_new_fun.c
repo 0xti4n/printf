262b7174ea4654dec35274fd56ddc9ce0070adaf
Saroco83 <estudialondres@gmail.com>
2019-10-29 19:22:02 +0000

    Fuction called to struct

---
+++ my_new_fun.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * new_fun - struct function selects the case
+ * @format: brings the array first position
+ * @list: brings the arguments to evaluate operation
+ * Return: integer value.
+ *
+ * Description: the function evaluates the parameters that comes from
+ * main function to uses the structur for select the correct case of
+ * format specifiers.
+ */
+
