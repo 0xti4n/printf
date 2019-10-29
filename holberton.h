1169f2a3cbcf707a0da883e8837183b94edc51ec
Cristian Gomez <1154@holbertonschool.com>
2019-10-29 19:07:59 +0000

    Prototype

--- holberton.h
+++ holberton.h
@@ -4,15 +4,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+
 typedef cases
 {
 	char *c;
 	int (*f)(va_list);
-} my_struct;
+}my_struct;
 
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _printf_char(va_list list);
 int _printf_str(va_list list);
 int (*new_fun(const char *format))(va_list list);
+
 #endif
