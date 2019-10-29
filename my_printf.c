08b18522d19d0bf048381565cf9b54dbe551b16a
Cristian Gomez <1154@holbertonschool.com>
2019-10-29 18:47:39 +0000

    Adding else condition

--- my_printf.c
+++ my_printf.c
@@ -9,35 +9,42 @@ int _printf(const char *format, ...)
 {
 	va_list list;
 	int i, len = 0;
-	int ch;
-
+	
 	va_start(list, format);
-
+	
 	if (format == NULL)
-	{
 		return (-1);
-	}
+	
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			i++;
+			
 			if (format[i] == '%')
 			{
 				_putchar(format[i])
-				len++;
+					len++;
 			}
-				if (format[i] == 'c')
-				{
+			
+			if (format[i] == 'c')
+			{
 				len = len + new_fun(format[i])(list);
-				}
-				else if (format[i] != '%')
-				{
-					_putchar('%');
-						_putchar(format[i]);
-						len + 2;
-				}
-
+			}
+			else if (format[i] != '%')
+			{
+				_putchar('%');
+				_putchar(format[i]);
+				len + 2;
+			}
 		}
+		else
+		{
+			_putchar(format[i]);
+			len++;
+		}
+		i++;
 	}
+	va_end(list);
+	return (len);
 }
