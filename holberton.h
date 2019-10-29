f244421f13907afcccf084f58d6b2eef488e13ac
jdrestre <1238@holbertonschool.com>
2019-10-29 19:05:32 +0000

    We add function prototypes to be evaluated

--- holberton.h
+++ holberton.h
@@ -12,5 +12,7 @@ typedef cases
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-
+int _printf_char(va_list list);
+int _printf_str(va_list list);
+int (*new_fun(const char *format))(va_list list);
 #endif
