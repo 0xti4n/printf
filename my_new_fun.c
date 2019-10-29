15d669e8fbf5870c9277b78549f8ae289f823712
Cristian Gomez <1154@holbertonschool.com>
2019-10-29 19:45:40 +0000

    Adding the loop

--- my_new_fun.c
+++ my_new_fun.c
@@ -1,11 +1,13 @@
 #include "holberton.h"
+
 /**
- * new_fun - struct function selects the case
- * @format: brings the array first position
- * @list: brings the arguments to evaluate
- *
- * Return: integer value.
- */
+* new_fun - struct function selects the case
+* @format: brings the array first position
+* @list: brings the arguments to evaluate
+*
+* Return: integer value.
+*/
+
 int (*new_fun(const char *format))(va_list list)
 {
 	my_struct array[] = {
@@ -13,4 +15,16 @@ int (*new_fun(const char *format))(va_list list)
 		{"s", _printf_str},
 		{NULL, NULL}
 	};
+
+	int i = 0;
+
+	while (array[i].c != NULL)
+	{
+		if (*(array[i].c) == *format)
+		{
+			return (array[i].f);
+		}
+		i++;
+	}
+	return (0);
 }
