cc187c1ccf91a975039d3b1838029f9ceb6d7857
jdrestre <1238@holbertonschool.com>
2019-10-29 20:05:17 +0000

    Creating the function that evaluates our case c, using _va_arg_

--- functions.c
+++ functions.c
@@ -1,25 +1,26 @@
 #include "holberton.h"
+
 /**
  * _printf_char - Used with c case
  * @list: Brings arguments
  * Return: Integer value
  */
+
 int _printf_char(va_list list)
+{
+	int c = va_arg(list, int);
 
-
-
-
-
-
-
+	_putchar(c);
+	return (1);
+}
 
 /**
  * _printf_str - Used in s case
  * @list: Brings arguments
  * Return: integer value
  */
+
 int _prinf_str(va_list list)
+{
 
-
-
-
+}
