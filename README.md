e3ee839c58867e9e52467a7752d7db29ecca5cc6
jdrestre <1238@holbertonschool.com>
2019-10-29 16:25:31 +0000

    I added comments on the _printf function that we are building

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # printf
-
+- I added comments on the _printf function that we are building and deleted some blank spaces that appeared according to the review with betty JDRZ
--- my_printf.c
+++ my_printf.c
@@ -1,13 +1,16 @@
 #include "holberton.h"
-
+/**
+ * _printf - Function construction that performs the same as printf in C
+ *
+ * @format: string for analyze
+ * Return: Integer
+ */
 int _printf(const char *format, ...)
 {
 	va_list list;
 	int i, len = 0;
 	int ch;
-	
-	va_start(list, format);
-	
 
-	
+	va_start(list, format);
+
 }
