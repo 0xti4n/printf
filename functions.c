e6a89ebade0cf8cf9dfb8f38b66178b76a914d70
Saroco83 <estudialondres@gmail.com>
2019-10-29 19:59:43 +0000

    Fuctions called to main

---
+++ functions.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _printf_char - Used with c case
+ * @list: Brings arguments
+ * Return: Integer value
+ */
+int _printf_char(va_list list)
+
+
+
+
+
+
+
+
+/**
+ * _printf_str - Used in s case
+ * @list: Brings arguments
+ * Return: integer value
+ */
+int _prinf_str(va_list list)
+
+
+
+
